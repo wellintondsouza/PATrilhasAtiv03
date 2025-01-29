0dcb0611c12f8526622f3347ec217d790d1bddc8
wellinton de Souza <168542172+wellintondsouza@users.noreply.github.com>
2025-01-29 09:36:11 -0300

    Create Questão A.cs

---
+++ Questão A.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Projeto1
+{
+	class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Digite o primeiro número:");
+			int num1 = int.Parse (Console.ReadLine());
+				
+			Console.WriteLine("Digite o segundo número:");
+			int num2 = int.Parse (Console.ReadLine());
+		
+			if (num1 > num2)
+			{
+				Console.WriteLine("O primeiro número é maior que o segundo!");
+			}
+			else 
+			{
+			
+				Console.WriteLine("O segundo número é maior!");
+			}
+			
+				  Console.WriteLine("Aperte qualquer botão para fechar...");
+        Console.ReadKey(); // Aguarda que o usuário pressione uma tecla
+		}
+	}
+}
